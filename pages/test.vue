bb941f01a9848152b6c73efa723ada88093b538d
Kennedy-Chidi <kennedychidi3@gmail.com>
2023-08-15 01:18:11 +0100

    ggood

---
+++ pages/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="runCode">Click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fullName: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  },
+  methods: {
+    runCode() {},
+  },
+};
+</script>
+
+<style></style>
